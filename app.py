0ceea9d2caa7ac3ce034312e1f725cb474ac5360
atufagaWOU <atufaga17@mail.wou.edu>
2021-01-27 03:49:54 +0000

    updated the POST endpoint to account for returns of 400, as well as updated the initial lists to include locations

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ app = Flask(__name__)
 
 activity_log = [
     {
-        'id': '0',
+        'id': 0,
         'user_id': 1,
         'username': 'john',
         'timestamp': datetime.utcnow(),
         'details': "Important stuff here",
+        'location': '/api/activities/0',
     },
     {
-        'id': '1',
+        'id': 1,
         'user_id': 2,
         'username': 'yoko',
         'timestamp': datetime.utcnow(),
         'details': "Even more important",
+        'location': '/api/activities/1',
     },
 
  ]
@@ -24,6 +26,7 @@ activity_log = [
 @app.route('/api/activities/<int:id>', methods=["GET"])
 def single(id):
     return  jsonify(activity_log[id])
+    
 
 @app.route('/api/activities', methods=["GET"])
 def all_activites():
@@ -31,8 +34,14 @@ def all_activites():
 
 @app.route('/api/activities', methods=["POST"])
 def new_activity():
+    if not request.json:
+        abort(400)
     new_act = request.get_json()
+    if 'user_id' not in new_act
+    or 'username' not in new_act
+    or 'details' not in new_act:
+        abort(400)
     new_act['id'] = 2
     new_act['location'] = url_for('single', id=2)
     new_act['timestamp'] = datetime.utcnow()
-    return jsonify(new_act)
+    return new_act,201
